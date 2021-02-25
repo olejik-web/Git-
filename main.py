06493b2cd7262b39fffcb29cf3a3fa46fc6c5d8c
olegator2004 <59178029+olejik-web@users.noreply.github.com>
2021-02-25 19:19:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from random import randrange
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        r = randrange(40, 200)
+        qp.drawEllipse(240, 240, r, r)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
